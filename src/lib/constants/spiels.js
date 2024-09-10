0beccbc7cfd476683f71099b8718a9edc2b6bc7b
Ronjay GA <rgacorda@Ronjays-MacBook-Pro.local>
2024-09-10 21:51:30 +0800

    feat: reorganized directories

---
+++ src/lib/constants/spiels.js
@@ -0,0 +1,27 @@
+/* eslint-disable sort-keys */
+const spiels = {
+	// Buttons
+	BUTTON_BACK: 'Back',
+	BUTTON_BOOK_NOW: 'Book Now',
+	BUTTON_CANCEL: 'Cancel',
+	BUTTON_CONTINUE: 'Continue',
+	BUTTON_CONFIRM: 'Confirm',
+	BUTTON_DEACTIVATE: 'Deactivate',
+	BUTTON_DEACTIVATE_ACCOUNT: 'Deactivate Account',
+	BUTTON_DELETE: 'Delete',
+	BUTTON_FILTER: 'Filter',
+	BUTTON_LOGIN: 'Login',
+	BUTTON_NEXT: 'Next',
+	BUTTON_PROCEED: 'Proceed',
+	BUTTON_RESET_FILTER: 'Reset Filters',
+	BUTTON_SAVE: 'Save',
+	BUTTON_SEND_RESET_REQUEST: 'Send Reset Request',
+	BUTTON_SET_APPOINTMENT: 'Set Appointment',
+	BUTTON_SIGN_IN: 'Sign In',
+	BUTTON_SIGN_UP: 'Sign Up',
+	BUTTON_SUBMIT: 'Submit',
+	BUTTON_VIEW: 'View',
+	BUTTON_VIEW_ALL: 'View all',
+};
+
+export default spiels;
---
+++ src/modules/home/screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HomeSubmitButton } from '../components/HomeSubmitButton';
+
+const HomeScreen = () => {
+	return <HomeSubmitButton />;
+};
+
+export default HomeScreen;
