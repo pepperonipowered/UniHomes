91eff849b2ce71bec7a19f840376387cf8db0ad6
JanRyanBallasio <98378424+JanBallasio@users.noreply.github.com>
2024-09-19 00:28:20 +0800

    feat : added terms and conditions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import HomeScreen from "@/modules/home/screens/HomeScreen";
 import NavBar from "@/components/navbar/Navbar";
+import Terms_Conditions from "@/modules/terms_and_conditions/screen/Terms_Conditions";
 
 export default function Home() {
-	return (
-		<div className=''>
-			<NavBar />
-            <HomeScreen />
-		</div>
-	);
+  return (
+    <div className="">
+      <NavBar />
+      <Terms_Conditions />
+    </div>
+  );
 }
