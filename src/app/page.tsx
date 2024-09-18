ea6c6f062ae931742a8d3454d619ef732f7a183b
SheddPugyao <20197474@s.ubaguio.edu>
2024-09-18 15:39:41 +0800

    fix: fixed favorites page grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import HomeScreen from "@/modules/home/screens/HomeScreen";
-import NavBar from "@/components/navbar/Navbar";
+// import HomeScreen from '@/modules/home/screens/HomeScreen';
+import NavBar from '@/components/navbar/Navbar';
+import FavoritesScreen from '@/modules/favorites/screens/FavoritesScreen';
 
 export default function Home() {
 	return (
 		<div className=''>
 			<NavBar />
-            <HomeScreen />
+			{/* <HomeScreen /> */}
+			<FavoritesScreen />
 		</div>
 	);
 }
